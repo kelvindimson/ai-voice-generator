2b27d3cdff0130a5b71df2066dc9f702a8ebbd4f
KelvinDimson <hello@kelvindimson.com>
2025-08-18 12:44:11 +1000

    chore: Cleaned up login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
 
  if (status === "loading") {
    return (
-     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100">
+     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br ">
        <Loader2 className="h-8 w-8 animate-spin text-gray-500" />
      </div>
    )
@@ -50,7 +50,7 @@ export default function LoginPage() {
 
  return (
    <>
-     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100">
+     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br">
        <Card className="w-full max-w-md shadow-none p-12">
          {!session ? (
            <>
